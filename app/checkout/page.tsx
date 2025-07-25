725b3f435672b228855d2e077d0b06c8430bee26
Rio-Prayoga <riopray38@gmail.com>
2025-07-25 20:27:47 +0700

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -149,7 +149,7 @@ export default function CheckoutPage() {
           </div>
 
           <div className="mt-4">
-            <h3 className="mb-2 font-medium">Lokasi Map Toko (simulasi):</h3>
+            <h3 className="mb-2 font-medium">Lokasi Map Toko:</h3>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253840.36370205287!2d110.24444504711418!3d-7.795579822419726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5780ac7b49b1%3A0x1de7f66d30c9c5ab!2sYogyakarta!5e0!3m2!1sen!2sid!4v1652708870767!5m2!1sen!2sid"
               width="100%"
